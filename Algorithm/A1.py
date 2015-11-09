6c60a59f0aaeb8d843c9b1d58d5eea8325c986d9
KadoreK <haltaskadir@gmail.com>
2015-11-09 20:45:54 +0200

    9-11-15

--- Algorithm/A1.py
+++ Algorithm/A1.py
@@ -21,25 +21,17 @@ def main():
 
     for x in range(1000,1000000):
         nler.append(x)
-        h = abs(real(x) - near(x)) / real(x)
-        print h;
+        h = abs(real(x) - near(x)) / float(real(x))
+        #print h
         hatalar.append(h)
         pass
 
-    #y ekseninin etiket değerinin ayarlanması
     plt.ylabel("Hata Payi")
-    #x ekseninin etiketinin değerinin ayarlanması
     plt.xlabel("n")  
-    # hesaplanan n sayıları ve bunlara karşılık gelen sürelerin(sn.  cinsinden)
-    # kalınlığı 2 piksel çizgi kullanarak grafiğin çizimi
    
     plt.plot(nler, hatalar,linewidth=2)   
-    # çizinlen grafiğin gösterimi
     plt.show()
-    # grafiğin eksenlerinin başlangıç ve bitiş değerlerinin ayarlanması
     plt.axis([1,len(nler),0,2])
-    # çalışan programın tüm işlemleri bitirdikten sonra ekranda birden
-    # kaybolmaması için giriş kodu
     input()
 
 
