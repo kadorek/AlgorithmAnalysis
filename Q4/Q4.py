4b8cbb8442686ba69c756b2c8027fa004b90dc0c
KadoreK <haltaskadir@gmail.com>
2015-11-12 14:10:52 +0200

    12-11-15

---
+++ Q4/Q4.py
@@ -0,0 +1,59 @@
+import sys
+import math
+import timeit
+import random
+
+
+
+def main():
+
+    nodes = []
+    node_ids = []
+
+    n = randint(2,1000)
+
+
+    for x in range(1,n):
+        nodes.append(x)
+        pass
+
+
+
+
+    pass
+
+
+
+if __name__ == "__main__":
+    sys.exit(int(main() or 0))
+
+
+class Question:
+
+    def __init__(self):
+        self.n = random.randint(20,1000)
+        self.m = random.randint(10,n / 2)
+        self.nodes = [].extend(range(1,self.n))
+        self.node_ids = []
+
+   
+    def HizliBul_Bul(self,node1, node2):
+        return node_ids[node1] == node_ids[node2]
+
+
+    def HizliBul_Birlestir(self,node1,node2):
+        eskiId = node_ids[node1]
+        for x in nodes:
+            if node_ids[x] == eskiId:
+                node_ids[x] = node_ids[node2]
+
+    def HizliBirlesim_Bul(self,node1,node2):
+        return HizliBirlesim_KokBul(node1) == HizliBirlesim_KokBul(node2)
+        pass
+    
+    def HizliBirlesim_KokBul(self,node):
+        while(node != node_ids[node]):
+            node = node_ids[node]
+        return node
+    
+
